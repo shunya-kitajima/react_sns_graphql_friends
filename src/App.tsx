0fc07eb71c517a5e341c7b46e810966ed1607f7c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-13 15:40:57 +0900

    set httpLink and authLink, client

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,23 @@ import Auth from './components/Auth'
 import MainPage from './components/MainPage'
 import styles from './App.module.css'
 
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql/',
+})
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : '',
+    },
+  }
+})
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 const App: React.FC = () => {
   return (
     <div className="App">
