aba53336b2bf90aba77581a8ae3709bd0a519a86
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-13 17:17:29 +0900

    import modules

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useQuery } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/client'
+import { Grid } from '@material-ui/core'
 import ExitToApp from '@material-ui/icons/ExitToApp'
+import {
+  UPDATE_FRIENDS,
+  UPDATE_FRIEND_REQUESTS,
+  GET_PROFILES,
+  GET_MYPROFILE,
+} from '../queries'
 import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
   const navigate = useNavigate()
+  const { data: dataMyProfile, error: errorMyProfile } = useQuery(
+    GET_MYPROFILE,
+    {
+      fetchPolicy: 'cache-and-network',
+    }
+  )
+  const { data: dataProfiles, error: errorProfiles } = useQuery(GET_PROFILES, {
+    fetchPolicy: 'cache-and-network',
+  })
 
   return (
     <div>
