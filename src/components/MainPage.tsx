6b960c1ff3a2154da6715c7a498d1658c4d7cfef
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-13 16:40:40 +0900

    import modules

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import ExitToApp from '@material-ui/icons/ExitToApp'
+import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
+  const navigate = useNavigate()
+
   return <div>MainPage</div>
 }
 
