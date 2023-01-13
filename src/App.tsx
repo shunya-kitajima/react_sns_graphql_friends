ed0aaab1a0be96d210ebf9dedf2b8ca26b169ed7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-13 15:41:29 +0900

    set router

--- src/App.tsx
+++ src/App.tsx
@@ -26,22 +26,16 @@ const client = new ApolloClient({
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.app__root}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/top" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   )
 }
 
